fef3c7a9d0f5362144aa416540ca862a6e35e365
Veronica Lourens <Veronica@lourens.cloud>
2022-03-22 19:02:42 +0000

    Add JS map method.

--- map.js
+++ map.js
@@ -6,14 +6,29 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let result = [];
+for (let num of nums) {
+    result.push(num * 2);
+}
+console.log(result);
 
 // Using map()
+const multipleByTwo = function(num) {
+    return num * 2;
+}
+const mapResults = nums.map(multipleByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
+const simplified = nums.map(function(num) { return num * 2 });
+console.log(simplified);
 
 
 // Simplfied w/ map() + arrow function
+const simplified2 = nums.map(num => num * 2);
+console.log(simplified2);
 
 
 // With objects:
@@ -37,3 +52,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentWithId = students.map(student => [student.name, student.id])
+console.log(studentWithId);
